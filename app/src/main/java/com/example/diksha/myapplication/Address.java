35c5350122ee68c0776309ccd7a1e87aa0461275
diksha694 <diksha6.94@gmail.com>
2015-07-23 13:12:24 +0530

    dropdown included

--- app/src/main/java/com/example/diksha/myapplication/Address.java
+++ app/src/main/java/com/example/diksha/myapplication/Address.java
@@ -1,13 +1,20 @@
 package com.example.diksha.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Address extends Fragment {
@@ -15,12 +22,40 @@ public class Address extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_address, null);
-//        Button button = (Button) view.findViewById(R.id.hbutton1);
-//        button.setText("horizontal");
-//        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
-//        String[] items = new String[]{"1", "2", "three"};
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-//        dropdown.setAdapter(adapter);
+        Spinner spinner = (Spinner)view.findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+                if (position == getCount()) {
+                    ((TextView)v.findViewById(android.R.id.text1)).setText("");
+                    //((TextView)v.findViewById(android.R.id.text1)).setTextColor(Color.parseColor("#FFFFFF"));
+                            ((TextView)v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+                }
+
+                return v;
+            }
+
+            @Override
+            public int getCount() {
+                return super.getCount()-1; // you dont display last item. It is used as hint.
+            }
+
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.add("Panjim");
+        adapter.add("Porvorim");
+        adapter.add("Mapusa");
+        adapter.add("Location");
+
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(adapter.getCount()); //display hint
+
         return view;
+
     }
 }
